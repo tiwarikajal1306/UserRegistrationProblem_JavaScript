bf92f91fc36d8f9c2910d12a811dbd4506d48827
kajal <tiwarikajal1306@gmail.com>
2020-08-03 08:31:18 +0530

    ADDED Validation of last name

--- UserRegistrationProblem.js
+++ UserRegistrationProblem.js
@@ -2,13 +2,21 @@ console.log("Welcome to User Registration Problem");
 let prompt = require('prompt-sync')();
 
 let firstNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2}");
-const name = prompt('Enter first name');
+const firstName = prompt('Enter first name');
 
-let checkFirstName = firstNameRegex.test(name);
+let checkFirstName = firstNameRegex.test(firstName);
 
 if(checkFirstName == true)
    console.log("FirstName is valid");
 else
    console.log("FirstName is Ivalid name");
 
+let lastNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2}");
+const lastName = prompt('Enter last name');
 
+let checkLastName = lastNameRegex.test(lastName);
+
+if(checkLastName == true)
+   console.log("LastName is valid");
+else
+   console.log("LastName is Ivalid name");
