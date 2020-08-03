25e3a4070d860c8df0bf09ee7975a6ab64d90ee5
kajal <tiwarikajal1306@gmail.com>
2020-08-03 08:16:27 +0530

    ADDED Validation of firstName

--- UserRegistrationProblem.js
+++ UserRegistrationProblem.js
@@ -1,1 +1,14 @@
 console.log("Welcome to User Registration Problem");
+let prompt = require('prompt-sync')();
+
+let firstNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2}");
+const name = prompt('Enter first name');
+
+let checkFirstName = firstNameRegex.test(name);
+
+if(checkFirstName == true)
+   console.log("FirstName is valid");
+else
+   console.log("FirstName is Ivalid name");
+
+
